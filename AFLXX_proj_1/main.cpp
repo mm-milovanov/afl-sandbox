1fd1b4808af0b7b5ad6ccf67c4d929c5a87a99eb
delta-ksi <delta.ksi42@gmail.com>
2025-03-25 00:08:41 +0300

    afl++-proj-1: impl binary search sqrt func with int overflow

--- AFLXX_proj_1/main.cpp
+++ AFLXX_proj_1/main.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 
-static int* ptr;
+int sqrt_int(int x) {
+	int left = 0, right = x, middle;
 
-void function_to_fuzz(char *str) {
-    if (str[0] == 'C')
-        if (str[1] == 'R')
-            if (str[2] == 'A')
-                if (str[3] == 'S')
-                    if (str[4] == 'H') {
-                        ptr = ((int*)0);
-                        *ptr = 0;
-                    }
+	while(left < right) {
+	    int middle = right + (left-right)/2;
+        // This can cause int overflow
+	    if ( middle * middle <= x )
+		    left = middle;
+	    else
+		    right = middle-1;
+	}
 
+	return left;
 }
 
-int main() { // int argc, char** argv
-    static char buf[100];
+int main(int, char**) {
+    static char buf[24];
     std::cin >> buf;
-    function_to_fuzz(buf);
+    std::cout << sqrt_int(atoi(buf)) << std::endl;
     return 0;
 }
